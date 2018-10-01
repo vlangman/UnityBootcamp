40ae63c9d468e57b6ae4283a54b5170ffbb80727
vlangman <vlangman00@gmail.com>
2018-10-01 21:05:25 +0200

    first push

---
+++ Day00/ex04/FlappyBird/Assets/Bird.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+	public Pipe pipe1;
+	public Pipe pipe2;
+	float gravity = 0.2f;
+	float jumpPower = 6.5f;
+	float velocity = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		pipe1.setCount(1);
+		pipe2.setCount(2);
+	}
+	
+	void FixedUpdate () {
+		transform.Translate(Vector3.down * Time.deltaTime * velocity);
+		velocity += gravity;
+
+		if (Input.GetKeyDown("up")){
+			velocity = -jumpPower;
+			Debug.Log("Space");
+		}
+	}
+
+
+	bool checkCollision(){
+		float birdScale = transform.localScale.x;
+		float pipeScale = pipe1.transform.localScale.x;
+		// if bird right side of bird (x + scale) bigger than left side wall( x - scale ) 
+		if (transform.position.x + birdScale > pipe1.transform.position.x - pipeScale){
+			//check top side and bot side collision
+			if (transform.position.y )
+
+		}
+		return true;
+	}
+}
---
+++ Day00/ex04/FlappyBird/Assets/Pipe.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour {
+
+	public int pipeCount =1;
+	public float speed = 2.5f;
+	bool startMove = false;
+	float Timer = 0.0f;
+	float startTime = 0.0f;
+	float waitTime;
+	int Score;
+	bool incScore = true;
+	Vector3 originalPos;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		originalPos = transform.position;
+		waitTime = 3.0f * pipeCount;
+		Score = 0;
+	}
+	
+	void FixedUpdate () {
+		Timer = Time.time;
+		if (Timer - startTime >= waitTime){
+			startMove = true;
+		}
+
+		if (startMove){
+			Move();
+		}else{
+			
+		}
+
+	}
+
+	void Move(){
+		transform.Translate(Vector3.left* speed * Time.deltaTime, Space.World);
+		
+		if (transform.position.x < 0.90 && incScore){
+			Score+=5;
+			Debug.Log("Score: " + Score);
+			Debug.Log("Time: " +  Mathf.RoundToInt(Time.realtimeSinceStartup) + "s");
+			incScore = false;
+			speed += 0.2f;
+		}
+
+
+		if (transform.position.x <= -3.44f){
+			waitTime = 5.0f;
+			startTime = Time.time;
+			startMove = false;
+			transform.position = originalPos;
+			incScore = true;
+		}
+	}
+
+	public void setCount(int count){
+		pipeCount = count;
+	}
+
+}
