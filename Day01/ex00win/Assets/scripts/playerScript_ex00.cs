c1aa395b3ef9055e769c40fe5161efa11e17678b
vlangman <vlangman00@gmail.com>
2018-10-02 19:26:53 +0200

    ex01 done 90%

---
+++ Day01/ex00win/Assets/scripts/playerScript_ex00.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerScript_ex00 : MonoBehaviour {
+
+	public Rigidbody2D rigidBody;
+
+	public Transform groundCheck;
+	private bool grounded = false;
+	private bool jump;
+	private float moveForce = 1000.0f;
+	float jumpForce = 3000f;
+	private float maxSpeed = 5f;
+
+	static GameObject currentObject;
+
+	// Use this for initialization
+	void Awake () {
+		jump = false;
+		currentObject = GameObject.FindWithTag("blue");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown("1")){
+			currentObject = GameObject.FindWithTag("red");;
+		} else if (Input.GetKeyDown("2")){
+			currentObject = GameObject.FindWithTag("blue");;
+		} else if (Input.GetKeyDown("3")){
+			currentObject = GameObject.FindWithTag("yellow");;
+		}
+		else if (Input.GetKeyDown("r")){
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+
+		grounded = Physics2D.Linecast(currentObject.transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+		if (Input.GetKeyDown("space") && grounded){
+			jump = true;
+		}
+	}
+
+	void FixedUpdate(){
+		rigidBody = currentObject.GetComponent<Rigidbody2D>();
+		float hSpeed = Input.GetAxis("Horizontal");
+
+		//set the velocity if not over max
+		if (hSpeed * rigidBody.velocity.x < maxSpeed)
+            rigidBody.AddForce(Vector2.right * hSpeed * moveForce * Time.deltaTime);
+
+    	//clamp the velocity
+        // check direction and set max speed to that dir
+        if (Mathf.Abs (rigidBody.velocity.x) > maxSpeed)
+            rigidBody.velocity = new Vector2(Mathf.Sign(rigidBody.velocity.x) * maxSpeed, rigidBody.velocity.y);
+
+
+        if (jump)
+        {
+            rigidBody.AddForce(new Vector2(0f, jumpForce) * Time.deltaTime);
+            jump = false;
+        }
+	}
+}
---
+++ Day01/ex00win/Assets/scripts/playerScript_ex01.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerScript_ex01 : MonoBehaviour {
+
+	public Rigidbody2D rigidBody;
+
+	public Transform groundCheck;
+	public bool grounded;
+	private bool jump;
+	public float moveForce = 1000.0f;
+	public float jumpForce = 3000f;
+	public float maxSpeed = 5f;
+	private BoxCollider2D m_Collider;
+	static GameObject currentObject;
+
+	// Use this for initialization
+	void Awake () {
+		grounded = false;
+		currentObject = GameObject.FindWithTag("blue");
+		jumpForce = 10000.0f; 
+		moveForce = 500f;
+		maxSpeed = 3f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown("1")){
+			currentObject = GameObject.FindWithTag("red");
+			jumpForce = 15000.0f;
+			moveForce = 1000f;
+			maxSpeed = 5f;
+
+		} else if (Input.GetKeyDown("2")){
+			currentObject = GameObject.FindWithTag("blue");
+			jumpForce = 10000.0f;
+			moveForce = 500f;
+			maxSpeed = 3f;
+		} else if (Input.GetKeyDown("3")){
+			currentObject = GameObject.FindWithTag("yellow");
+			jumpForce = 20000.0f;
+			moveForce = 1500f;
+			maxSpeed = 7f;
+		}
+		else if (Input.GetKeyDown("r")){
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+
+		if (Input.GetKeyDown("space") && grounded){
+			jump = true;
+			grounded = false;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+    {
+    	Debug.Log("collision");
+         foreach (ContactPoint2D contact in collision.contacts) {
+            // print(contact.thisCollider.name + " hit " + contact.otherCollider.name);
+            Debug.DrawRay(contact.point, contact.normal, Color.red, 0.5f);
+            Debug.Log(contact.normal);
+            //if normal for contact point -1 ((-90))
+            if (contact.normal.x == 0.0f && contact.normal.y == 1.0f){
+            	grounded = true;
+            }
+        }
+    }
+
+	void FixedUpdate(){
+		rigidBody = currentObject.GetComponent<Rigidbody2D>();
+		float hSpeed = Input.GetAxis("Horizontal");
+
+		//set the velocity if not over max
+		if (hSpeed * rigidBody.velocity.x < maxSpeed)
+            rigidBody.AddForce(Vector2.right * hSpeed * moveForce * Time.deltaTime);
+
+    	//clamp the velocity
+        // check direction and set max speed to that dir
+        if (Mathf.Abs (rigidBody.velocity.x) > maxSpeed)
+            rigidBody.velocity = new Vector2(Mathf.Sign(rigidBody.velocity.x) * maxSpeed, rigidBody.velocity.y);
+
+
+        if (jump)
+        {
+            rigidBody.AddForce(new Vector2(0f, jumpForce) * Time.deltaTime);
+            jump = false;
+        }
+	}
+}
